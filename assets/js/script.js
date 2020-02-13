5fca05880f0e47805b75490806fd5378f329e77f
Megan Coppins <meganlcoppins@gmail.com>
2020-02-13 07:08:00 -0700

    local storage commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
-console.log(moment().hours());
+var $currentDay = $("#currentDay");
+
+
+setTime();
+var interval = setInterval(setTime, 1000);
+function setTime(){
+    var date = moment().format("dddd, MMMM Do YYYY, k:mm:ss");
+    $currentDay.innerHTML = date;
+}
+// store event inputs
+
+$(".saveBtn").on("click", function(e){
+    e.preventDefault();
+    var toDoList = $(this).siblings(".col-10").val();
+    console.log(toDoList);
+    localStorage.setItem("toDoList", toDoList);
+    // var storedToDoList = localStorage.getItem("toDoList")
+    $("#textArea").innerHTMl = localStorage.getItem("toDoList");
+
+});
+
