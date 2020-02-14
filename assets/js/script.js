12f851ff877eea199c4b3a8482267b78e637bf87
Megan Coppins <meganlcoppins@gmail.com>
2020-02-13 20:03:54 -0700

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,3 @@
-// THEN the current day is displayed at the top of the calendar
-//1. display current time on cal DONE
-
-// THEN I am presented with timeblocks for standard business hours
-//2. dynamically display textarea and associated btn.. make this a function to later go back and add class for color later
-// var arrtime=[9,10,]
-// function create planner(){
-// 	//hour =9am arrtime[i]
-// 	if(currentime-arrtime[i]==0){
-// 		p.attr("class","ontimestyle");
-// 	}
-// }
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-//create a save function to store to local storage (would just change to each time as a var and set)
-// THEN the text for that event is saved in local storage
-//once we get the hour then we set it to the specific text area
-// WHEN I refresh the page
-// THEN the saved events persist
-
-// var obj = {
-//   name: "Tucker",
-//   age: 55
-// };
-
-// console.log(obj["name"]);
-
-// for (var key in obj) {
-//   console.log(key, obj[key]);
-// }
-
 $(document).ready(function() {
   var $currentDay = $("#currentDay");
   var notes = getNotes();
